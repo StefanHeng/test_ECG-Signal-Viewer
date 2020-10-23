50c5db0bf81e5796160384729c7e8a5f32694904
Stefan Heng <43276957+SpongeBobBang@users.noreply.github.com>
2020-10-23 09:29:17 -0400

    New: test h5 file data stroing structure & local simple plot

--- test_data_read.py
+++ test_data_read.py
@@ -8,28 +8,62 @@
 # import plotly.express as px
 # import pandas as pd
 import pathlib
-import os
+# import os
 import h5py
 import json
-import numpy as np
+# import numpy as np
 
 # from dash.dependencies import Input, Output  # , State
 
 # from scipy import stats
 
+# enum SignalType { UNUSED = 0, ECG = 1, BOX = 2, PRESSURE = 3, ANALOG = 4, STIM = 5 };
 
 DATA_PATH = pathlib.Path("/Users/stefanh/Documents/UMich/Research/ECG-Signal-Viewer/data")
 
 selected_record = "LOG_DHR50526570_09000e6f-001.h5"
-selected_file = "00000003.log"
 
-record = h5py.File(DATA_PATH.joinpath(selected_record), 'r')
-header = json.loads(record[selected_file].attrs['metadata'])
+if __name__ == "__main__":
+    record = h5py.File(DATA_PATH.joinpath(selected_record), 'r')
+    files = list(record.keys())  # contains only log files
+    print("Keys for a `record`: ")
+    print(files)
+    print()
 
-attrs = record[selected_file].attrs
-print(attrs)
-# print(attrs["metadata"])
+    selected_file = files[3]
+    attrs = record[selected_file].attrs  # attributes of the log
+    # print(attrs)
+    # # print(attrs["metadata"])
+    # for i in attrs:
+    #     print(i)
+    print("metadata for each `file` of a `record`: ")
+    print(list(attrs))  # contains only metadata
+    print()
 
-for i in attrs:
-    print(i)
+    header = json.loads(record[selected_file].attrs['metadata'])
+    globalHeader = header.copy()
+    globalHeader['sigheader'] = []  # cos it's a long list of dictionaries
+    print("global/shared metadata for all `sigheader`s of a `file`: ")
+    print(globalHeader)
+    print()
+    print('metadata for a few `sigheader`s: ')
+    for i in range(5):
+        print(header['sigheader'][i])
+    # print("Signal 0 has this data: " + str(header['sigheader'][0]))
+    print()
 
+    print("metadata for a `record`: ")
+    print(list(record.attrs))  # attributes of the entire surgery
+    annotations = json.loads(record.attrs['annotations'])
+    print("header for the list of `annotation`: ")
+    print(annotations[0])
+    print()
+    print("the 2nd element in the list of `annotations` is protocol: ")
+    print(annotations[1])
+    print()
+
+    num = 10
+    print(f"the first {num}/{len(annotations)} elements of a `record`'s `annotation`: ")
+    for i in range(2, num+1):
+        print(annotations[i])
+    print()
---
+++ test_graph_data.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# import base64
+# import io
+# import dash
+# import dash_core_components as dcc
+# import dash_html_components as html
+# import plotly.graph_objs as go
+# import plotly.express as px
+# import pandas as pd
+import pathlib
+# import os
+import h5py
+import json
+# import numpy as np
+
+# from dash.dependencies import Input, Output  # , State
+
+# from scipy import stats
+
+import matplotlib.pyplot as plt
+
+from test_data_read import DATA_PATH, selected_record
+
+if __name__ == "__main__":
+    record = h5py.File(DATA_PATH.joinpath(selected_record), 'r')
+    files = list(record.keys())  # contains only log files
+    print(files)
+    print()
+
+    selected_file = files[3]  # This is the file name, as dict key, not the file itself
+    dataset = record[selected_file]
+    print(type(dataset), dataset.dtype, dataset.shape)
+    print(dataset.shape[0] * dataset.shape[1], dataset.size)
+    print()
+
+    lead = dataset[0]
+    print(type(lead), lead.dtype, lead.shape)
+
+    plt.plot(lead[:3000])
+    plt.xlabel('sample #')
+    plt.ylabel('mV')
+    plt.title('lead signal')
+    plt.show()
