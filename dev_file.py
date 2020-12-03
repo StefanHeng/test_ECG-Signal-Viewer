f67e5c5b2c6f6a723c436eecc82dd540f41438ee
Stefan Heng <43276957+SpongeBobBang@users.noreply.github.com>
2020-12-02 23:03:44 -0500

    Update: requirements.txt; Syntactic sugar & organize strings

--- dev_file.py
+++ dev_file.py
@@ -1,4 +1,4 @@
 import pathlib
 
-DATA_PATH = pathlib.Path("/Users/stefanh/Documents/UMich/Research/ECG-Signal-Viewer/data")
+DATA_PATH = pathlib.Path("/Users/stefanh/Documents/UMich/Research/EcgViz/ECG-Signal-Viewer/data")
 selected_record = "LOG_DHR50526570_09000e6f-001.h5"
--- ecg_dev_run-1-1.py
+++ ecg_dev_run-1-1.py
@@ -9,28 +9,42 @@ from dev_file import *
 from ecg_app import EcgApp
 
 D_RNG_INIT = [
-    [0, 100000],  # 100s
+    [0, 100000],
     [-4000, 4000]  # -4V to 4V
 ]
-# PRESERVE_UI_STATE = 'keep'  # string val assigned arbitrarily
+
 ID_GRA = 'graph'
-IG_STOR_D_RNG = '_display_range'  # Contains dictionary of each display range
-D_CONF = {
-    'responsive': True,
-    'scrollZoom': True,
-    'modeBarButtonsToRemove': ['lasso2d', 'select2d', 'autoScale2d', 'toggleSpikelines',
-                               'hoverClosestCartesian', 'hoverCompareCartesian'],
-    'displaylogo': False
-}
+ID_STOR_D_RNG = '_display_range'  # Contains dictionary of each display range
+CONF = dict(  # Configuration for figure
+    responsive=True,
+    scrollZoom=True,
+    modeBarButtonsToRemove=['lasso2d', 'select2d', 'autoScale2d', 'toggleSpikelines',
+                            'hoverClosestCartesian', 'hoverCompareCartesian'],
+    displaylogo=False
+)
 ID_BTN_FIXY = 'btn_fix_yaxis'
 ID_IC_FIXY = 'ic_fix_yaxis'
 ID_DIV_OPN = 'div_options'
-CLSNM_IC_LK = 'fas fa-lock'
-CLSNM_IC_LKO = 'fas fa-lock-open'
+CNM_IC_LK = 'fas fa-lock'  # Font Awesome
+CNM_IC_LKO = 'fas fa-lock-open'
 ID_STOR_IS_FIXY = 'id_is_yaxis_fixed'
 
-T = 'type'
+# Syntactic sugar
+T = 'type'  # For pattern matching callbacks
 I = 'index'
+D = 'data'  # Keyword for Dash and plotly
+F = 'figure'
+CNM = 'className'
+
+CNM_HD = 'header'
+CNM_HDTT = 'header_title'
+TXT_HD = 'Ecg Viz'  # Text shown in header
+CNM_MNBD = 'body_main'
+ID_DIV_PLTS = 'div_plots'
+CNM_DIV_LD = 'div_lead'
+TXT_NM_LD = 'name-lead'
+CNM_DIV_FIG = 'div_figure'
+CNM_BTN = 'btn'
 
 ecg_app = EcgApp(__name__)
 ecg_app.set_curr_record(DATA_PATH.joinpath(selected_record))
@@ -46,25 +60,25 @@ idxs_fig = range(8)
 
 app.layout = html.Div([
     # dcc.Store(id=IG_STOR_D_RNG, data={idx: D_RNG_INIT for idx in idxs_fig}),
-    html.Div(className="app-header", children=[
-        html.Div('Ecg Viz', className="app-header_title")
+    html.Div(className=CNM_HD, children=[
+        html.Div(TXT_HD, className=CNM_HDTT)
     ]),
 
-    html.Div(className='main-body', children=[
-        html.Div(id='plots', children=[
-            html.Div(className='lead-block', children=[
-                dcc.Store(id={T: IG_STOR_D_RNG, I: idx}, data=D_RNG_INIT),
-                html.P(ecg_app.curr_lead_nms[idx], className='name-lead'),
-                html.Div(className='figure-container', children=[
+    html.Div(className=CNM_MNBD, children=[
+        html.Div(id=ID_DIV_PLTS, children=[
+            html.Div(className=CNM_DIV_LD, children=[
+                dcc.Store(id={T: ID_STOR_D_RNG, I: idx}, data=D_RNG_INIT),
+                html.P(ecg_app.curr_lead_nms[idx], className=TXT_NM_LD),
+                html.Div(className=CNM_DIV_FIG, children=[
                     html.Div(className=ID_DIV_OPN, children=[
-                        html.Div(html.Button(id={T: ID_BTN_FIXY, I: idx}, className='button', n_clicks=0, children=[
-                            html.I(id={T: ID_IC_FIXY, I: idx}, className='fas fa-lock-open')
+                        html.Div(html.Button(id={T: ID_BTN_FIXY, I: idx}, className=CNM_BTN, n_clicks=0, children=[
+                            html.I(id={T: ID_IC_FIXY, I: idx}, className=CNM_IC_LKO)
                         ])),
                     ]),
                     dcc.Graph(
                         id={T: ID_GRA, I: idx},
                         figure=ecg_app.get_lead_fig(idx, D_RNG_INIT),
-                        config=D_CONF,
+                        config=CONF,
                         style=dict(
                             width='95%',
                             height='90%',
@@ -83,10 +97,14 @@ def get_last_changed_ids():
     return [p['prop_id'] for p in dash.callback_context.triggered][0]
 
 
+def match(id_str):  # Syntactic sugar
+    return {T: id_str, I: MATCH}
+
+
 @app.callback(
-    Output({T: IG_STOR_D_RNG, I: MATCH}, 'data'),
-    [Input({T: ID_GRA, I: MATCH}, 'relayoutData')],
-    [State({T: IG_STOR_D_RNG, I: MATCH}, 'data')],
+    Output(match(ID_STOR_D_RNG), D),
+    [Input(match(ID_GRA), 'relayoutData')],
+    [State(match(ID_STOR_D_RNG), D)],
     prevent_initial_call=True)
 def update_limits(relayout_data, d_range):
     if relayout_data is None:
@@ -101,30 +119,30 @@ def update_limits(relayout_data, d_range):
 
 
 @app.callback(
-    Output({T: ID_GRA, I: MATCH}, 'figure'),
-    [Input({T: IG_STOR_D_RNG, I: MATCH}, 'data'),
-     Input({T: ID_BTN_FIXY, I: MATCH}, 'n_clicks')],
-    [State({T: ID_GRA, I: MATCH}, 'figure'),
-     State({T: ID_GRA, I: MATCH}, 'id')])
-def update_figure(d_range, n_clicks, f, id_d):
+    Output(match(ID_GRA), F),
+    [Input(match(ID_STOR_D_RNG), D),
+     Input(match(ID_BTN_FIXY), 'n_clicks')],
+    [State(match(ID_GRA), F),
+     State(match(ID_GRA), 'id')])
+def update_figure(d_range, n_clicks, fig, id_d):
     changed_id = get_last_changed_ids()
-    if IG_STOR_D_RNG in changed_id:  # Only 1 input change is needed each time
+    if ID_STOR_D_RNG in changed_id:  # Only 1 input change is needed each time
         x, y = ecg_app.get_lead_xy_vals(id_d[I], d_range)
-        f['data'][0]['x'] = x
-        f['data'][0]['y'] = y
+        fig[D][0]['x'] = x  # First plot/figure on the graph
+        fig[D][0]['y'] = y
     elif ID_BTN_FIXY in changed_id:
-        f['layout']['yaxis']['fixedrange'] = n_clicks % 2 == 1
-    return f
+        fig['layout']['yaxis']['fixedrange'] = n_clicks % 2 == 1
+    return fig
 
 
 @app.callback(
-    Output({T: ID_IC_FIXY, I: MATCH}, 'className'),
-    Input({T: ID_BTN_FIXY, I: MATCH}, 'n_clicks'))
+    Output(match(ID_IC_FIXY), CNM),
+    Input(match(ID_BTN_FIXY), 'n_clicks'))
 def update_fix_yaxis_icon(n_clicks):
     if n_clicks % 2 == 0:  # Init with yaxis unlocked
-        return CLSNM_IC_LKO
+        return CNM_IC_LKO
     else:
-        return CLSNM_IC_LK
+        return CNM_IC_LK
 
 
 # @profile
